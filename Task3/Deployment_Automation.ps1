8bbb45fe729c2fb686d3d04cdbc7cb34544a0d67
Ajagba Emmanuel <79184436+Emmyblaze12@users.noreply.github.com>
2026-06-18 17:53:28 +0100

    Create Deployment_Automation.ps1

---
+++ Task3/Deployment_Automation.ps1
@@ -0,0 +1,24 @@
+# =====================================
+# Task 3
+# Deployment Automation
+# =====================================
+
+$BaseUrl = "https://bi.company.com/api"
+$Token = "TOKEN"
+
+$Headers = @{
+    Authorization = "Bearer $Token"
+    "Content-Type" = "application/json"
+}
+
+$Payload = @{
+    SourcePath = "/Dev"
+    TargetPath = "/Test"
+    Overwrite = $true
+} | ConvertTo-Json
+
+Invoke-RestMethod `
+    -Uri "$BaseUrl/deployment/promote" `
+    -Method POST `
+    -Headers $Headers `
+    -Body $Payload
